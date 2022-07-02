592f45091b6e8b3cee553a36ed732dd54154cce3
hjl <2764881569@qq.com>
2022-07-02 19:11:32 +0800

    Updates

--- assets/index.018885aa.js
+++
@@ -1,1 +0,0 @@
-import{j as e,F as r}from"./index.60d67183.js";function a(){return e(r,{children:"Live"})}export{a as default};
--- assets/index.58a29938.js
+++ assets/index.58a29938.js
@@ -1,4 +1,4 @@
-import{s as l,r as o,a,F as d,j as i,L as p}from"./index.60d67183.js";import{g as c,S as x,W as f}from"./request.34fa031d.js";const g=l.div`
+import{s as l,r as o,a,F as d,j as i,L as p}from"./index.37b4a61b.js";import{g as c,S as m,W as x}from"./request.38f401e6.js";const f=l.div`
   width: 98%;
   margin: 4px auto;
   padding-bottom: 44px;
@@ -8,7 +8,7 @@ import{s as l,r as o,a,F as d,j as i,L as p}from"./index.60d67183.js";import{g a
     width: 50%;
     display: flex;
     .videos-box {
-      height: 176px;
+      height: 194px;
       border-radius: 2px;
       margin-left: 4px;
       margin-right: 5px;
@@ -16,6 +16,7 @@ import{s as l,r as o,a,F as d,j as i,L as p}from"./index.60d67183.js";import{g a
       background-color: rgba(60, 60, 60, 0.1);
       position: relative;
       .videos-img {
+        position: relative;
         img {
           border-radius: 2px;
           width: 100%;
@@ -27,12 +28,11 @@ import{s as l,r as o,a,F as d,j as i,L as p}from"./index.60d67183.js";import{g a
           justify-content: space-around;
           position: absolute;
           height: 22px;
-          bottom: 60px;
-          font-size: 10px;
-          line-height: 20px;
+          bottom: 0;
+          font-size: 12px;
+          line-height: 22px;
           left: 0;
           right: 0;
-          z-index: 10;
           padding: 2px;
           border-radius: 2px;
           background: rgba(30, 30, 30, 0.36);
@@ -43,7 +43,8 @@ import{s as l,r as o,a,F as d,j as i,L as p}from"./index.60d67183.js";import{g a
         text-align: left;
         padding-left: 8px;
         font-size: 12px;
-        font-weight: 400;
+        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        font-weight: 600;
         /* 实现标题显示两行，溢出的用省略号表示 */
         text-overflow: ellipsis;
         display: -webkit-box;
@@ -69,4 +70,4 @@ import{s as l,r as o,a,F as d,j as i,L as p}from"./index.60d67183.js";import{g a
       }
     }
   }
-`,{Toast:m}=f;function u(){const[s,n]=o.exports.useState(!0),[t,r]=o.exports.useState([]);return o.exports.useEffect(()=>{(async()=>{let{data:e}=await c();r([...e]),n(!1)})()},[]),a(d,{children:[i(x,{}),a(g,{children:[i(m,{show:s,icon:"loading",children:"\u52A0\u8F7D\u4E2D..."}),t&&t.map(e=>i("div",{className:"videos-flex",children:i(p,{to:`/hot/video${e.id}`,children:a("div",{className:"videos-box",children:[a("div",{className:"videos-img",children:[i("img",{src:e.img,alt:""}),a("div",{className:"info",children:[i("i",{className:"iconfont icon-bofangqi-bofangxiaodianshi"}),i("span",{children:e.bofang}),i("i",{className:"iconfont icon-jianyi"}),i("span",{children:e.pinglun}),i("span",{children:e.time})]})]}),i("div",{className:"title",children:i("span",{children:e.title})}),i("div",{className:"up",children:i("span",{children:e.up})})]})},e.id)},e.id))]})]})}export{u as default};
+`,{Toast:g}=x;function u(){const[s,n]=o.exports.useState(!0),[t,r]=o.exports.useState([]);return o.exports.useEffect(()=>{(async()=>{let{data:e}=await c();r([...e]),n(!1)})()},[]),a(d,{children:[i(m,{}),a(f,{children:[i(g,{show:s,icon:"loading",children:"\u52A0\u8F7D\u4E2D..."}),t&&t.map(e=>i("div",{className:"videos-flex",children:i(p,{to:`/campus/video${e.id}`,children:a("div",{className:"videos-box",children:[a("div",{className:"videos-img",children:[i("img",{src:e.img,alt:""}),a("div",{className:"info",children:[i("i",{className:"iconfont icon-bofangqi-bofangxiaodianshi"}),i("span",{children:e.bofang}),i("i",{className:"iconfont icon-jianyi"}),i("span",{children:e.pinglun}),i("span",{children:e.time})]})]}),i("div",{className:"title",children:i("span",{children:e.title})}),i("div",{className:"up",children:i("span",{children:e.up})})]})},e.id)},e.id))]})]})}export{u as default};
