f5cf4ca020dc8386c0128b674d513b6b8192e37c
hjl <2764881569@qq.com>
2022-07-12 19:04:46 +0800

    bilibili-page

--- src/pages/Home/Campus/index.jsx
+++ src/pages/Home/Campus/index.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { Skeleton } from 'antd-mobile'
 import SetMovie from '../SetMovie'
 import VideoList from '../VideoList'
+import { getVideosList } from '../store/actionCreators'
 
-export default function Recommend() {
+const Campus = (props) => {
+  const { videosList, enterLoading } = props
+  const { getVideoListDispatch } = props
+
+  useEffect(() => {
+    getVideoListDispatch()
+  }, [])
+
+  const renderVideo = () => {
+    return (
+      <>
+        <SetMovie />
+        <VideoList videosList={videosList} />
+      </>
+    )
+  }
 
   return (
     <>
-      <SetMovie />
-      <VideoList />
+      { enterLoading ? (<Skeleton.Paragraph lineCount={25} animated />) : renderVideo() }
     </>
   )
 }
+
+// state 状态树
+const mapStateToProps = (state) => {
+  return {
+    enterLoading: state.home.enterLoading,
+    videosList: state.home.videosList
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getVideoListDispatch() {
+      dispatch(getVideosList())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Campus))
--- src/pages/Home/VideoList/index.jsx
+++ src/pages/Home/VideoList/index.jsx
@@ -1,60 +1,20 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
 import { Wrapper } from './style'
-import { Skeleton } from 'antd-mobile'
 import VideoItem from '@/components/VideoItem'
-import { getVideosList } from '../store/actionCreators'
 
-const VideoList = (props) => {
-  const { videosList, enterLoading } = props
-  const { getVideoListDispatch } = props
-
-  useEffect(() => {
-    getVideoListDispatch()
-  }, [])
-
-  const renderVideo = () => {
-    return (
-      <>
-        {
-          videosList && videosList.map(
-            video => (
-              <VideoItem key={video.id} video={video} />
-            )
-          )
-        }
-      </>
-    )
-  }
+const VideoList = ({videosList}) => {
 
   return (
     <Wrapper>
-      { enterLoading ? (<Skeleton.Paragraph lineCount={25} animated />) : renderVideo() }
-      {/* {
+      {
         videosList && videosList.map(
           video => (
             <VideoItem key={video.id} video={video} />
           )
         )
-      } */}
+      }
     </Wrapper>
   )
 }
 
-// state 状态树
-const mapStateToProps = (state) => {
-  return {
-    enterLoading: state.home.enterLoading,
-    videosList: state.home.videosList
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getVideoListDispatch() {
-      dispatch(getVideosList())
-    }
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(React.memo(VideoList))
+export default VideoList
