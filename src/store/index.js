319b5c1758d91e1fdb72e026d8f31af76269c057
hjl <2764881569@qq.com>
2022-07-18 23:58:17 +0800

    redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,17 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 // 组件  中间件redux-thunk   数据
-import thunk from 'redux-thunk' // 异步数据管理
 import reducer from './reducer'
+import thunk from 'redux-thunk'  // 异步数据管理
+import logger from 'redux-logger'  // 让 redux 调试更优秀
 
 const composeEnhancers = 
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(reducer, 
-  composeEnhancers(
-    applyMiddleware(thunk)
+  // 合并成一个中间件对象
+  compose(
+    composeEnhancers(applyMiddleware(thunk)),
+    applyMiddleware(logger)
   )
 )
 
