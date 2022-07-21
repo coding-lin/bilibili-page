cedc74d014d1a472eb30ceb682ed8d33dbc65ded
hjl <2764881569@qq.com>
2022-07-21 12:03:59 +0800

    配置请求对象

--- src/api/config.js
+++ src/api/config.js
@@ -7,4 +7,12 @@ const axiosInstance = axios.create({
   baseURL: baseUrl
 })
 
+// 响应拦截
+axiosInstance.interceptors.response.use(
+  res => res.data,
+  err => {
+    console.log(err, '网络错误~~')
+  }
+)
+
 export { axiosInstance }
--- src/api/request.js
+++ src/api/request.js
@@ -3,6 +3,9 @@ import { axiosInstance } from "./config";
 export const getVideosListRequest = () => 
   axiosInstance.get('/videoslist')  // 首页视频列表
 
+export const getSuggestVideosListRequest = () =>
+  axiosInstance.get('/videoslist')
+
 export const getBannersListRequest = () => 
   axiosInstance.get('/bannerslist')  // 会员购 banners 
 
