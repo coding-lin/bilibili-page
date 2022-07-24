0a89b90c58b092e51f4197900abfc25c32c5cccb
hjl <2764881569@qq.com>
2022-07-24 11:37:10 +0800

    增加searchReducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as homeReducer } from '@/pages/Home/store/index'
+import { reducer as searchReducer } from "@/pages/Home/HomeSearch/store/index"
 import { reducer as vipReducer } from '@/pages/Vip/store/index'
 
 export default combineReducers({
   home: homeReducer,
-  vip: vipReducer
+  vip: vipReducer,
+  search: searchReducer
 })
