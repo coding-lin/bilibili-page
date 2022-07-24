45b81e4ea4596d0367d004ff5b042bb6bb6d68bd
hjl <2764881569@qq.com>
2022-07-24 11:38:38 +0800

    添加搜索建议api

--- src/api/request.js
+++ src/api/request.js
@@ -3,8 +3,8 @@ import { axiosInstance } from "./config";
 export const getVideosListRequest = () => 
   axiosInstance.get('/videoslist')  // 首页视频列表
 
-export const getSuggestVideosListRequest = () =>
-  axiosInstance.get('/videoslist')
+export const getSuggestListRequest = () =>
+  axiosInstance.get('/suggestlist')  // 首页搜索建议列表
 
 export const getBannersListRequest = () => 
   axiosInstance.get('/bannerslist')  // 会员购 banners 
