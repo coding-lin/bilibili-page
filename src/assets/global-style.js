7ed36614c8e0ef561d3f079e83b505593b1963aa
hjl <2764881569@qq.com>
2022-07-24 14:46:48 +0800

    global-style.js

--- src/assets/global-style.js
+++ src/assets/global-style.js
@@ -1,4 +1,6 @@
 export default {
   "background-color": "rgba(50, 50, 50, 0.06)",
-  "search_bar-color": "rgba(50, 50, 50, 0.08)"
+  "search_bar-color": "rgba(50, 50, 50, 0.08)",
+  "border-color": "rgba(50, 50, 50, 0.2)",
+  "loading-color": "rgba(0, 150, 250, 0.8)"
 }
