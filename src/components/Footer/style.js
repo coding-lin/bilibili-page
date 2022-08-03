23c114addbe61e25789fbe8b0d3457cbf9d2bc5f
hjl <2764881569@qq.com>
2022-08-03 11:58:42 +0800

    Footer

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,15 +1,16 @@
 import styled from 'styled-components'
+import { px2rem } from '@/assets/global-style'
 
 export const FooterWrapper = styled.div`
   width: 100%;
-  height: 50px;
+  height: ${px2rem(50)};
   background-color: #fff;
   position: fixed;
   bottom: 0;
   left: 0;
   right: 0;
   z-index: 999;
-  border-top: 1px solid rgba(50, 50, 50, 0.1);
+  border-top: ${px2rem(1)} solid rgba(50, 50, 50, 0.1);
   display: flex;
   a {
     flex: 1;
@@ -22,9 +23,10 @@ export const FooterWrapper = styled.div`
       color: pink;
     }
     i {
-      font-size: 1.6em;
+      font-size: ${px2rem(21)};
     }
     span {
+      font-size: ${px2rem(12)};
       color: #000;
     }
   }
