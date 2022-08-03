96cd98eabaa4cf17f7e7cce7a1cd96a549f35fac
hjl <2764881569@qq.com>
2022-08-03 12:00:14 +0800

    GoodsItem

--- src/components/GoodsItem/index.jsx
+++ src/components/GoodsItem/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import LazyLoad from 'react-lazyload'
 import classnames from 'classnames'
 import bilibili from '@/assets/images/bilibili.jpeg'
+import { Wrapper } from './style'
 
 const GoodsItem = ({good}) => {
   const [isColl, setIsColl] = useState(false)
@@ -11,40 +12,42 @@ const GoodsItem = ({good}) => {
   }
 
   return (
-    <div className="good-box" key={good.id}>
-      <div className="good-img">
-        <LazyLoad
-          placeholder={<img width="100%" 
-          height="100%" src={bilibili}/>}
-        >
-          <img src={good.img} />
-        </LazyLoad>
-      </div>
-      <div className="title">
-        {good.info ? <span>{good.info}</span> : ''}
-        <span>{good.title}</span>
-      </div>
-      <div className="rank_time">
-        {good.rank ? <span>{good.rank}</span> : ''}
-        {good.time ? <span>{good.time}</span> : ''}
-      </div>
-      <div className="price_coll">
-        <span>¥{good.price}</span>
-        <span>
-          <i
-            className={classnames(
-              'iconfont', 
-              {'icon-aixin3': !isColl}, 
-              {'icon-aixin1': isColl}, 
-              {'active': isColl}
-            )}
-            onClick={() => changeColl()}
+    <Wrapper>
+      <div className="good-box" key={good.id}>
+        <div className="good-img">
+          <LazyLoad
+            placeholder={<img width="100%" 
+            height="100%" src={bilibili}/>}
           >
-          </i>
-          <span>{isColl ? good.collection + 1 : good.collection}</span>
-        </span>
+            <img src={good.img} />
+          </LazyLoad>
+        </div>
+        <div className="title">
+          {good.info ? <span>{good.info}</span> : ''}
+          <span>{good.title}</span>
+        </div>
+        <div className="rank_time">
+          {good.rank ? <span>{good.rank}</span> : ''}
+          {good.time ? <span>{good.time}</span> : ''}
+        </div>
+        <div className="price_coll">
+          <span>¥{good.price}</span>
+          <span>
+            <i
+              className={classnames(
+                'iconfont', 
+                {'icon-aixin3': !isColl}, 
+                {'icon-aixin1': isColl}, 
+                {'active': isColl}
+              )}
+              onClick={() => changeColl()}
+            >
+            </i>
+            <span>{isColl ? good.collection + 1 : good.collection}</span>
+          </span>
+        </div>
       </div>
-    </div>
+    </Wrapper>
   )
 }
 
---
+++ src/components/GoodsItem/style.js
@@ -0,0 +1,75 @@
+import styled from "styled-components"
+import { px2rem } from '@/assets/global-style'
+
+export const Wrapper = styled.div`
+  .good-box { 
+    width: 100%;
+    break-inside: avoid;
+    margin-bottom: ${px2rem(10)};
+    border-radius: ${px2rem(4)};
+    box-shadow: rgba(255, 255, 255, 0.2) ${px2rem(2)} ${px2rem(2)} ${px2rem(2)};
+    background-color: #fff;
+    .good-img {
+      width: 100%;
+      img {
+        border-radius: ${px2rem(4)};
+        width: 100%;
+      }
+    }
+    .title {
+      padding: ${px2rem(2)};
+      >span:first-child {
+        background: rgba(50, 50, 50, 0.1);
+        border-radius: ${px2rem(4)};
+        font-size: ${px2rem(10)};
+        padding: ${px2rem(2)};
+      }
+      >span:nth-child(2) {
+        font-style: oblique;
+        font-size: ${px2rem(14)};
+        font-weight: 600;
+        padding: ${px2rem(2)};
+      }
+    }
+    .rank_time {
+      display: flex;
+      justify-content: space-around;
+      padding: ${px2rem(2)};
+      >span:first-child {
+        background-color: orange;
+        border-radius: ${px2rem(4)};
+        padding: ${px2rem(1)};
+        font-size: ${px2rem(12)};
+      }
+      >span:nth-child(2) {
+        font-size: ${px2rem(12)};
+        border-radius: ${px2rem(4)};
+        padding: ${px2rem(1)};
+        background-color: skyblue;
+      }
+    }
+    .price_coll {
+      display: flex;
+      justify-content: space-between;
+      padding: ${px2rem(2)};
+      >span:first-child {
+        padding: ${px2rem(8)};
+        font-size: ${px2rem(18)};
+        font-weight: 600;
+      }
+      >span:nth-child(2) {
+        padding: ${px2rem(6)};
+        i {
+          font-size: ${px2rem(20)};
+          padding: ${px2rem(2)};
+          &.active {
+            color: red;
+          }
+        }
+        span {
+          padding: ${px2rem(2)};
+        }
+      }
+    }
+  }
+`
