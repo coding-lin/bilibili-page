a2f489e5d9481e99558e91e3bd49e6d6092bcaef
hjl <2764881569@qq.com>
2022-08-03 15:40:29 +0800

    VideoItem

--- src/components/VideoItem/index.jsx
+++ src/components/VideoItem/index.jsx
@@ -1,41 +1,38 @@
 import React from 'react'
 import LazyLoad from 'react-lazyload'
-import { Link } from 'react-router-dom'
 import bilibili from '@/assets/images/bilibili.jpeg'
+import { Wrapper } from './style'
 
 const VideoItem = ({video}) => {
 
   return (
-    <div className='videos-flex' key={video.id}>
-      <Link
-        to={`/recommend/video${video.id}`}
-        key={video.id}
-      >
-        <div className='videos-box'>
-          <div className="videos-img">
-            <LazyLoad
-              placeholder={<img width="100%" 
-              height="100%" src={bilibili}/>}
-            >
-              <img src={video.img} />
-            </LazyLoad>
-            <div className="info">
-              <i className='iconfont icon-bofangqi-bofangxiaodianshi'></i>
-              <span>{video.bofang}</span>
-              <i className='iconfont icon-pinglun'></i>
-              <span>{video.pinglun}</span>
-              <span>{video.time}</span>
-            </div>
-          </div>
-          <div className='title'>
-            <span>{video.title}</span>
-          </div>
-          <div className='up'>
-            <span>{video.up}</span>
+    <Wrapper>
+      <div className="video-box" key={video.id}>
+        <div className="video-img">
+          <LazyLoad
+            placeholder={<img width="100%" 
+            height="100%" src={bilibili}/>}
+          >
+            <img src={video.img} width="100%" height="100%" />
+          </LazyLoad>
+          <div className="data">
+            <i className='iconfont icon-bofangqi-bofangxiaodianshi'></i>
+            <span>{video.bofang}</span>
+            <i className='iconfont icon-pinglun'></i>
+            <span>{video.pinglun}</span>
+            <span>{video.time}</span>
           </div>
         </div>
-      </Link>
-    </div>
+        <div className="video-info">
+          <div className='title'>
+            {video.title}
+          </div>
+          <div className='up'>
+            {video.up}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
   )
 }
 
---
+++ src/components/VideoItem/style.js
@@ -0,0 +1,63 @@
+import styled from "styled-components"
+import { px2rem } from '@/assets/global-style'
+
+export const Wrapper = styled.div`
+  .video-box {
+    padding: ${px2rem(5)};
+    border-radius: ${px2rem(2)};
+    .video-img {
+      position: relative;
+      img {
+        border-radius: ${px2rem(2)};
+      }
+      .data {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: ${px2rem(22)};
+        line-height: ${px2rem(22)};
+        color: #fff;
+        background: rgba(50, 50, 50, 0.5);
+        border-radius: ${px2rem(2)};
+        display: flex;
+        justify-content: space-around;
+        i {
+          font-size: ${px2rem(18)};
+        }
+        span {
+          font-size: ${px2rem(13)};
+        }
+      }
+    }
+    .video-info {
+      position: relative;
+      height: ${px2rem(76)};
+      background: #fff;
+      .title {
+        padding: ${px2rem(2)};
+        font-size: ${px2rem(14)};
+        /* 实现标题显示两行，溢出的用省略号表示 */
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        display: -moz-box;
+        -moz-line-clamp: 2;
+        -moz-box-orient: vertical;
+        overflow-wrap: break-word;
+        word-break: break-all;
+        white-space: normal;
+        overflow: hidden;
+      }
+      .up {
+        position: absolute;
+        font-size: ${px2rem(12)};
+        padding: ${px2rem(2)};
+        border: ${px2rem(1)} solid pink;
+        border-radius: ${px2rem(2)};
+        bottom: ${px2rem(4)};
+        left: ${px2rem(4)};
+      }
+    }
+  }
+`
