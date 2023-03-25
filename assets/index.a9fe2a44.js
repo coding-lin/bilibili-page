409910c3d77ff4365e6954991052cf01d6c5f340
hjl <2764881569@qq.com>
2023-03-25 13:36:00 +0800

    Updates

--- assets/index.a9fe2a44.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index.952ae5c2.js";function r(){return i("div",{children:"Dynamic"})}export{r as default};
--- assets/request.8de268d9.js
+++
@@ -1,1 +0,0 @@
-import{h as s}from"./index.952ae5c2.js";const e=()=>s.get("/videoslist"),g=()=>s.get("/suggestlist"),o=()=>s.get("/bannerslist"),i=()=>s.get("/goodslist");export{o as a,i as b,e as c,g};
