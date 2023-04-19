e39aadc93106799423b8cb6c7a38d18dfb098af0
hjl <2764881569@qq.com>
2023-04-19 18:06:07 +0800

    feat:获取url参数

--- src/pages/Vip/GoodDetail/index.jsx
+++ src/pages/Vip/GoodDetail/index.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { connect } from 'react-redux'
 import { Toast, Badge } from 'antd-mobile'
 import { SendOutline } from "antd-mobile-icons"
 import SharePopup from "@/components/SharePopup"
+import { getGoodsList } from '../store/actionCreators'
 import './index.scss'
 
-const GoodDetail = () => {
+const GoodDetail = (props) => {
+  const { goodsList } = props
+  const { getGoodListDispatch } = props
   const navigate = useNavigate()
+  const { id } = useParams()
   const [visible, setVisible] = useState(false)
+  console.log(id, '------')
 
   const addCart = () => {
     Toast.show('加车成功')
   }
+
+  useEffect(() => {
+    getGoodListDispatch()
+  }, [])
 
   return (
     <div className='good-detail-wrapper'>
@@ -50,4 +60,18 @@ const GoodDetail = () => {
   )
 }
 
-export default GoodDetail
+const mapStateToProps = (state) => {
+  return {
+    goodsList: state.vip.goodsList
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getGoodListDispatch() {
+      dispatch(getGoodsList())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(GoodDetail))
